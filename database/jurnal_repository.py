43892c2220652d5c37e92ddc6244152d4ba084b6
MHusainiR <husaini4848@gmail.com>
2026-06-17 20:39:49 +0700

    Update Riwayat.py

--- database/jurnal_repository.py
+++ database/jurnal_repository.py
@@ -35,3 +35,29 @@ def save_journal(
 
     cur.close()
     conn.close()
+
+def get_latest_journals(limit=5):
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT
+            id,
+            content,
+            emotion,
+            mood_score,
+            risk_level,
+            sentiment,
+            created_at
+        FROM journals
+        ORDER BY created_at DESC
+        LIMIT %s
+    """, (limit,))
+
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return rows
