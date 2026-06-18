11ffac888ad35190fdbcdb8f81f56440c841da7f
Fahim <akufahim575@gmail.com>
2026-06-18 20:19:07 +0700

    commit

--- database/connection.py
+++ database/connection.py
@@ -1,10 +1,13 @@
 import psycopg2
+import streamlit as st
 
 def get_connection():
+    # Menggunakan st.secrets agar password Anda tetap aman di Cloud
     conn = psycopg2.connect(
-        host="localhost",
-        database="safespace",
-        user="postgres",
-        password="admin123"
+        host=st.secrets["db.exngosuvyhtlcbacldlh.supabase.co"],
+        database=st.secrets["postgres"],
+        user=st.secrets["postgres"],
+        password=st.secrets["aisafespace1234"],
+        port=st.secrets["5432"]
     )
     return conn
