7a94822f8368bfd43521c463fb7b0adaadddd989
Fahim <akufahim575@gmail.com>
2026-06-18 21:07:45 +0700

    commit

--- database/connection.py
+++ database/connection.py
@@ -2,12 +2,13 @@ import psycopg2
 import streamlit as st
 
 def get_connection():
-    # Menggunakan st.secrets agar password Anda tetap aman di Cloud
+    # Gunakan kunci umum (DB_HOST, DB_NAME, dll)
+    # Streamlit akan mencarinya di menu "Secrets"
     conn = psycopg2.connect(
-        host=st.secrets["db.exngosuvyhtlcbacldlh.supabase.co"],
-        database=st.secrets["postgres"],
-        user=st.secrets["postgres"],
-        password=st.secrets["aisafespace1234"],
-        port=st.secrets["5432"]
+        host=st.secrets["DB_HOST"],
+        database=st.secrets["DB_NAME"],
+        user=st.secrets["DB_USER"],
+        password=st.secrets["DB_PASSWORD"],
+        port=st.secrets["DB_PORT"]
     )
     return conn
